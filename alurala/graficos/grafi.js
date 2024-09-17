6d8dfb2c8ac76221253e71ef37b450be66fe4ad5
haliferg <130460824+haliferg@users.noreply.github.com>
2024-09-17 08:08:21 -0300

    Add files via upload

---
+++ alurala/graficos/grafi.js
@@ -0,0 +1,30 @@
+<script type="module" src="graficos/informacoesGlobais.js"></script>
+
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+
+        const res = await fetch(url)
+
+        const dados = await res.json()
+
+        console.log(dados);
+
+    }
+
+    visualizarInformacoesGlobais()
+
+    async function vizualizarInformacoesGlobais() {
+        const res = await fetch(url)
+
+        const dados await res.json()
+
+        const paragrafo document.createElement('p')
+
+        paragrafo.classList.add('graficos-container__texto')
+
+       paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+        console.log(paragrafo)
+      }
+      
+      vizualizarInformacoesGlobais()
